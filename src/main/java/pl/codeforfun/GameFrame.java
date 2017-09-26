b175bacc846e54f644622795a831fb4ca843a4ff
Lukasz <lstaszew@gmail.com>
2017-09-26 23:50:58 +0200

    Update GameFrame.java

--- src/main/java/pl/codeforfun/GameFrame.java
+++ src/main/java/pl/codeforfun/GameFrame.java
@@ -1,27 +1,9 @@
 package pl.codeforfun;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.Insets;
-import java.awt.Panel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-
 
 /**
  * @author LS
